d2b0dcc96ae86e45fcb34e463304619c9e3e8e43
jonasborges93 <jonas.bgs93@gmail.com>
2024-09-22 13:04:48 -0300

    feat: Removed SASS from the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
-import './App.scss'
+import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css'
+
+function Header(){
+
+    return(
+        <>
+            <header className="header">
+                <div className="container">
+                    <h1 className="header__title">App Previsão do Tempo Dev</h1>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
