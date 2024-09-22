92e417fb303fae1f3bd43b7d37945fbc0b844cfb
jonasborges93 <jonas.bgs93@gmail.com>
2024-09-22 13:12:50 -0300

    feat: Added the footer to the project

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+
+function Footer (){
+    return(
+        <footer className="footer">
+            <div className="container">
+                <p className="footer__text">Todos os direitos reservados | <a href="#" className="footer__link" target='_blank'>Jonas Borges</a></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
